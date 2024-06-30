2f0874516077ade89c1282d5822840cd7e318eaa
haryomidey <oladiipoayomide2021@gmail.com>
2024-06-30 09:55:36 +0100

    created add new teacher page

--- src/components/admin/user management/NewTeacher.tsx
+++ src/components/admin/user management/NewTeacher.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from '../Header'
 import { FaUser, FaCamera } from 'react-icons/fa'
+import { IoIosAdd } from "react-icons/io";
 
 const NewTeacher = () => {
     return (
-        <main  className="w-full h-screen ">
+        <main  className="w-full min-h-screen pb-10 bg-white">
             <Header headerName="New teacher" />
 
-            <div className="relative pt-6 px-12 w-full h-full bg-white">
+            <div className="relative pt-6 px-12 w-full min-h-full">
                 <h1 className='text-2xl font-semibold'>Add teacher</h1>
 
                 <div className='mt-6'>
@@ -23,7 +24,62 @@ const NewTeacher = () => {
                     </div>
 
                     <div className='mt-8'>
-                        <h2>Personal Information</h2>
+                        <h2 className='text-xl font-semibold mb-4'>Personal Information</h2>
+
+                        <form>
+                            <div className='grid gap-2'>
+                                <label htmlFor="">Title</label>
+                                <select name="" id="" className='w-[fit-content] p-2 border'>
+                                    <option value="">Mr</option>
+                                    <option value="">Mrs</option>
+                                    <option value="">Miss</option>
+                                </select>
+                            </div>
+                            <div className='grid grid-cols-2 gap-8 mt-5'>
+                                <div className='grid gap-2'>
+                                    <label htmlFor="">First Name</label>
+                                    <input
+                                        type="text"
+                                        className='p-2 w-full border rounded'
+                                        placeholder='Enter your first name'
+                                    />
+                                </div>
+                                <div className='grid gap-2'>
+                                    <label htmlFor="">Last Name</label>
+                                    <input
+                                        type="text"
+                                        className='p-2 w-full border rounded'
+                                        placeholder='Enter your last name'
+                                    />
+                                </div>
+                                <div className='grid gap-2'>
+                                    <label htmlFor="">Dob</label>
+                                    <input
+                                        type="date"
+                                        className='p-2 w-full border rounded'
+                                    />
+                                </div>
+                                <div className='grid gap-2'>
+                                    <label htmlFor="">Gender</label>
+                                    <div className='flex items-center gap-8'>
+                                        <p className='flex items-center gap-2'>
+                                            <input type="radio" name="gender" id="" />
+                                            <span>Male</span>
+                                        </p>
+                                        <p className='flex items-center gap-2'>
+                                            <input type="radio" name="gender" id="" />
+                                            <span>Female</span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className='col-span-2 flex items-center justify-end text-white'>
+                                    <button className='flex items-center gap-2 px-5 py-2 rounded bg-[#2B5BFC]'>
+                                        <IoIosAdd />
+                                        <p>Add</p>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
