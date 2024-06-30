6de6fa808db722524dce2c9552c7c76b239e4bf4
haryomidey <oladiipoayomide2021@gmail.com>
2024-06-30 10:03:20 +0100

    created add new teacher page

--- src/components/admin/settings/AdminSettings.tsx
+++ src/components/admin/settings/AdminSettings.tsx
@@ -1,13 +1,44 @@
-import React from "react";
 import Header from "../Header";
-import ComingSoon from "../../ComingSoon";
 
 function AdminSettings() {
+
+  const comps = [
+    {
+      title: 'My Profile',
+    },
+    {
+      title: 'Security',
+    },
+    {
+      title: 'Data Export',
+    },
+    {
+      title: 'Communication',
+    },
+    {
+      title: 'Notification',
+    },
+    {
+      title: 'Delete Account',
+    }
+  ]
+
+
+  
+
   return (
-    <div>
+    <div className="w-full h-screen">
       <Header headerName="Settings" />
-      <div className="relative">
-        <ComingSoon />
+      <div className="relative w-full h-[90%] p-6">
+        <div className="w-full h-full flex shadow-md rounded-md">
+          <ul className="w-[20%] border-r py-6 pl-5 pr-8 flex flex-col gap-4">
+            {
+              comps.map((comp, index) => (
+                <li key={index}>{comp.title}</li>
+              ))
+            }
+          </ul>
+        </div>
       </div>
     </div>
   );
